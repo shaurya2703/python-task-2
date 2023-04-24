e0780d83b6d89511b6d5bcb598e72d3c8d751638
Shaurya Khanna <shauryakhanna@Shauryas-MacBook-Pro.local>
2023-04-24 21:42:14 +0530

    Changed code to use mongoDB instead of SQLALchemy

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from flask import Flask, request, jsonify
-from models import db,User, Organisation, OrganisationUser, serialize_organisation, serialize_user
-from flask_pymongo import PyMongo
+from models import db,User, Organisation, OrganisationUser
+from pymongo import MongoClient
+from bson import ObjectId
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+
 app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
 app.config['SECRET_KEY'] = "random string"
-db.init_app(app)
-mongo = PyMongo(app)
+
+client = MongoClient('localhost',27017)
+db = client['user_management_app']
+users = db['users']
+organisations = db['organisations']
+users_organisations = db['users_organisations']
 
 
 
@@ -19,15 +24,12 @@ def hello():
 def add_user():
     name = request.json['name']
     email = request.json['email']
-    new_user = User(name=name,email=email)
-    if 'id' in request.json:
-        id = request.json['id']
-        new_user.id = id
+    user_id = request.json['id'] if 'id' in request.json else ObjectId()
+    new_user = User(id=user_id,name=name,email=email)
 
-    db.session.add(new_user)
-    db.session.commit()
+    result = users.insert_one(new_user.to_dict())
 
-    return jsonify(serialize_user(user=new_user))
+    return f"Added user with ID: {result.inserted_id}"
 
 @app.route("/list_users",methods=['GET'])
 def list_users():
@@ -35,43 +37,53 @@ def list_users():
     limit = request.args.get('limit', 10, type=int)
     offset = request.args.get('offset', 0, type=int)
 
-    query = User.query
+    query = {}
     if name_filter:
-        query = query.filter(User.name.like(f'%{name_filter}%'))
+        query['name'] = {'$regex': f'{name_filter}.*', '$options': 'i'}
 
-    users = query.offset(offset).limit(limit).all()
-    total_count = query.count()
+    # Get total count of records
+    total_count = users.count_documents(query)
 
-    response = {
-        'users': [ serialize_user(user) for user in users],
-        'total_count': total_count
-    }
+    # Perform pagination
+    cursor = users.find(query).skip(offset).limit(limit)
 
+    # Build response
+    user_list = [user for user in cursor]
+    for user in user_list:
+        user['_id'] = str(user['_id']) # Convert ObjectId to string
+        user['id'] = str(user['id'])
+    response = {'users': user_list, 'total_count': total_count}
     return jsonify(response)
 
 @app.route('/users/<string:name>')
 def get_user(name):
     print(name)
-    user = User.query.filter_by(name=name).first()
-    if user is None:
-        return jsonify({'message': 'User not found'}), 404
+    # Find user by name
+    user = users.find_one({'name': name})
 
-    return jsonify({
-        'id': user.id,
-        'name': user.name,
-        'email': user.email
-    })
+    # If user not found, return 404
+    if user is None:
+        return jsonify({'error': 'User not found'}), 404
+
+    # Convert ObjectId to string
+    user['_id'] = str(user['_id'])
+    user['id'] = str(user['id'])
+
+    # Return user as JSON response
+    return jsonify(user)
 
 @app.route("/organisations",methods=['POST'])
 def add_organisation():
     name = request.json['name']
 
-    new_org = Organisation(name)
+    org_id = request.json['id'] if 'id' in request.json else ObjectId()
+    new_org = Organisation(id=org_id,name=name)
 
-    db.session.add(new_org)
-    db.session.commit()
+    result = organisations.insert_one(new_org.to_dict())
 
-    return jsonify(serialize_organisation(new_org))
+    return f"Added organisation with ID: {result.inserted_id}"
+
+    
 
 @app.route("/list_organisations",methods=['GET'])
 def list_organisations():
@@ -79,60 +91,77 @@ def list_organisations():
     limit = request.args.get('limit', 10, type=int)
     offset = request.args.get('offset', 0, type=int)
 
-    query = Organisation.query
+    query = {}
     if name_filter:
-        query = query.filter(Organisation.name.like(f'%{name_filter}%'))
+        query['name'] = {'$regex': f'{name_filter}.*', '$options': 'i'}
 
-    organisations = query.offset(offset).limit(limit).all()
-    total_count = query.count()
+    # Get total count of records
+    total_count = organisations.count_documents(query)
 
-    response = {
-        'users': [ serialize_organisation(organisation) for organisation in organisations],
-        'total_count': total_count
-    }
+    # Perform pagination
+    cursor = organisations.find(query).skip(offset).limit(limit)
 
+    # Build response
+    org_list = [org for org in cursor]
+    for org in org_list:
+        org['_id'] = str(org['_id']) # Convert ObjectId to string
+        org['id'] = str(org['id'])
+    response = {'organisations': org_list, 'total_count': total_count}
     return jsonify(response)
 
 @app.route('/organisations/<string:name>')
 def get_organisation(name):
     print(name)
-    org = Organisation.query.filter_by(name=name).first()
+    org = organisations.find_one({'name':name})
     if org is None:
-        return jsonify({'message': 'User not found'}), 404
+        return jsonify({'message': 'Organisation not found'}), 404
 
-    return jsonify(serialize_organisation(org))
+    org['_id'] = str(org['_id'])
+    org['id'] = str(org['id'])
+
+    return jsonify(org)
 
 
-@app.route('/organisations/<int:org_id>/permissions', methods=['POST'])
-def update_org_permissions(org_id):
-    data = request.json
-    users = data.get('users', [])
-    access_level = data.get('access_level')
+@app.route('/permissions', methods=['POST'])
+def update_org_permissions():
+    user_id = request.json['user_id']
+    org_permissions = request.json['org_permissions']
 
-    org = Organisation.query.get_or_404(org_id)
+    # Update permissions for each organization
+    for org_permission in org_permissions:
+        # Get organization ID and access level from organization permission object
+        org_id = org_permission.get('org_id')
+        access_level = org_permission.get('access_level')
 
-    for user_id in users:
-        user_org = OrganisationUser.query.filter_by(user_id=user_id, org_id=org.id).first()
-        if user_org:
-            user_org.access_level = access_level
-        else:
-            user_org = OrganisationUser(user_id=user_id, org_id=org.id, access_level=access_level)
-            db.session.add(user_org)
+        # Check if organization exists in database
+        organisation = organisations.find_one({'_id': ObjectId(org_id)})
+        if organisation is None:
+            return jsonify({'error': 'Organization not found'}), 404
 
-    db.session.commit()
+        # Check if user exists in database
+        user = users.find_one({'_id': ObjectId(user_id)})
+        if user is None:
+            return jsonify({'error': 'User not found'}), 404
 
-    return jsonify({'message': 'Permissions updated successfully'}), 200
+        # Update user's permissions for organization in users_organisations collection
+        users_organisations.update_one(
+            {'user_id': ObjectId(user_id), 'org_id': ObjectId(org_id)},
+            {'$set': {'access_level': access_level}},
+            upsert=True)
+
+    return jsonify({'success': 'Permissions updated successfully'}), 200
 
 @app.route('/organisations/<int:org_id>/users/<int:user_id>', methods=['DELETE'])
 def remove_user_permission(org_id, user_id):
-    organisation = Organisation.query.get(org_id)
-    user = User.query.get(user_id)
+    organisation = organisations.find_one({'id':org_id})
+    org_json = jsonify(organisation)
+    user = users.find_one({'id':user_id})
+    user_json = jsonify(user)
     if organisation and user:
-        user_org = OrganisationUser.query.filter_by(user_id=user_id, org_id=org_id).first()
+        user_org = users_organisations.find_one({'user_id':user_id, 'org_id':org_id})
         if user_org:
-            db.session.delete(user_org)
-            db.session.commit()
-            return jsonify({'message': f'User {user.name} has been removed from organization {organisation.name}.'}), 200
+            users_organisations.delete_one({'_id':user_org['_id']})
+            return jsonify({'message': f'User {user_json} has been removed from organization {org_json}.'}), 200
         else:
             return jsonify({'error': 'User does not have access to this organization.'}), 404
     else:
@@ -141,5 +170,5 @@ def remove_user_permission(org_id, user_id):
 
 if __name__ == "__main__":
     with app.app_context():
-        db.create_all()
+        # db.create_all()
         app.run(debug=True,port=8000)
--- models.py
+++ models.py
@@ -2,46 +2,29 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
-    name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), nullable=False, unique=True)
-
-    def __init__(self, name, email):
+class User:
+    
+    def __init__(self,id,name, email):
+        self.id = id
         self.name = name
         self.email = email
 
-def serialize_user(user):
+    def to_dict(self):
         return {
-            'id': user.id,
-            'name': user.name,
-            'email': user.email
-        }              
+            'id': self.id,
+            'name': self.name,
+            'email': self.email,
+        }  
 
 
-class Organisation(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False, unique=True)
-
-    def __init__(self, name):
+class Organisation():
+    
+    def __init__(self, id,name):
+        self.id = id
         self.name = name
 
-def serialize_organisation(organisation):
-    return {
-        'id':organisation.id,
-        'name':organisation.name
-    }
-
-class OrganisationUser(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    org_id = db.Column(db.Integer, db.ForeignKey('organisation.id'))
-    access_level = db.Column(db.Enum('READ', 'WRITE', 'ADMIN'), nullable=False)
-
-    user = db.relationship(User, backref=db.backref('user_organisations', cascade='all, delete-orphan'))
-    organisation = db.relationship(Organisation, backref=db.backref('user_organisations', cascade='all, delete-orphan'))
-
-    def __init__(self, user_id, org_id, access_level):
-        self.user_id = user_id
-        self.org_id = org_id
-        self.access_level = access_level
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+        }      
